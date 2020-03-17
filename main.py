26fb08d737a464048dad325170f0498da99f4b46
OctaveLauby <octavelauby@gmail.com>
2020-03-17 19:24:12 -0400

    Add prints to time computation time

--- main.py
+++ main.py
@@ -4,10 +4,26 @@
 if __name__ == "__main__":
     import numpy as np
     from olfractals import Screen
+    from time import time
 
     screen = Screen()
     screen.open()
     line = np.array([[0, 1], [1, 1], [1, 0], [2, 1]])
-    screen.compute_fit_params(line)
+
+    t = time()
+    print("Prepare line...", end="")
+    line = np.array([[i//2, (i+1)//2] for i in range(int(1e2))])
+    print(f" done in {time()-t}s")
+
+    t = time()
+    print("Fit line...", end="")
+    params = screen.compute_fit_params(line)
+    from pprint import pprint; pprint(params)
+    print(f" done in {time()-t}s")
+
+    t = time()
+    print("Draw line...", end="")
     screen.draw_line(line)
+    print(f" done in {time()-t}s")
+
     screen.wait_close()
