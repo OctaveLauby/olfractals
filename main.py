acebb71876844152f7bec60ade7dcc5980212fe7
OctaveLauby <octavelauby@gmail.com>
2020-03-18 02:55:03 -0400

    Draw a fractal in main

--- main.py
+++ main.py
@@ -3,26 +3,51 @@
 
 if __name__ == "__main__":
     import numpy as np
-    from olfractals import Screen
+    from olfractals import Fractal, Screen
     from time import time
 
-    screen = Screen()
-    screen.open()
-    line = np.array([[0, 1], [1, 1], [1, 0], [2, 1]])
 
+    # ---------------------------------------------------------------------- #
+    # Fractal
+
+    print("Define fractal")
+    sin = np.sin(60 * np.pi / 180)
+    def b_oper():
+        line = np.array([[0, 0], [1/3, 0], [0.5, sin/3], [2/3, 0], [1, 0]])
+        return [line], []
+    iter_n = 8
+
+    fractal = Fractal(b_oper, as_basis=True)
+    for k, v in fractal.growth_info.items():
+        print(f"\t| {k}={v}")
+    print(f"\t| iterations={iter_n}")
+    print(f"\t| segments={fractal.evaluate_growth(iter_n)}")
+
+    # Compute fractal
     t = time()
-    print("Prepare line...", end="")
-    line = np.array([[i//2, (i+1)//2] for i in range(int(1e2))])
+    print("Prepare fractal lines...", end="")
+    lines = fractal.compute_b(iter_n)
+    line = lines[0]
     print(f" done in {time()-t}s")
 
+
+    # ---------------------------------------------------------------------- #
+    # Display
+
+    screen = Screen()
+
+    # Make points fit the screen
     t = time()
     print("Fit line...", end="")
     params = screen.compute_fit_params(line)
-    from pprint import pprint; pprint(params)
     print(f" done in {time()-t}s")
+    for k, v in params.items():
+        print(f"\t| {k}={v}")
 
+    # Display
     t = time()
     print("Draw line...", end="")
+    screen.open()
     screen.draw_line(line)
     print(f" done in {time()-t}s")
 
